6037b1219c1733f607dde84dedf9f28f2709c126
Artem <a.yuzhankov@e-u.edu.ua>
2026-08-27 13:55:35 +0300

    P2. Task Manager initialization

--- ktor/src/test/kotlin/ServerTest.kt
+++ ktor/src/test/kotlin/ServerTest.kt
@@ -1,36 +1,89 @@
 package com.example
 
-import io.ktor.client.request.*
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
-import io.ktor.server.testing.*
-import kotlin.test.Test
-import kotlin.test.assertContains
-import kotlin.test.assertEquals
+import io.ktor.http.formUrlEncode
+import io.ktor.server.testing.testApplication
+import kotlin.test.*
 
 class ServerTest {
 
     @Test
     fun `test root endpoint`() = testApplication {
-        // loads default configuration
-        application {
-            configureRouting()
-        }
-        // verify server root returns 200
+        application { configureRouting() }
         assertEquals(HttpStatusCode.OK, client.get("/").status)
     }
 
     @Test
     fun `test new endpoint`() = testApplication {
-        application {
-            configureRouting()
-        }
+        application { configureRouting() }
 
         val response = client.get("/test1")
 
         assertEquals(HttpStatusCode.OK, response.status)
         assertEquals("html", response.contentType()?.contentSubtype)
         assertContains(response.bodyAsText(), "Hello From Ktor")
+    }
+
+    @Test
+    fun tasksCanBeFoundByPriority() = testApplication {
+        application { configureRouting() }
+
+        val response = client.get("/tasks/byPriority/Medium")
+        val body = response.bodyAsText()
+
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertContains(body, "Mow the lawn")
+        assertContains(body, "Paint the fence")
+    }
+
+    @Test
+    fun invalidPriorityProduces400() = testApplication {
+        application { configureRouting() }
+
+        val response = client.get("/tasks/byPriority/Invalid")
+        assertEquals(HttpStatusCode.BadRequest, response.status)
+    }
+
+    @Test
+    fun unusedPriorityProduces404() = testApplication {
+        application { configureRouting() }
+
+        val response = client.get("/tasks/byPriority/Vital")
+        assertEquals(HttpStatusCode.NotFound, response.status)
+    }
+
+    @Test
+    fun newTasksCanBeAdded() = testApplication {
+        application { configureRouting() }
+
+        val response1 = client.post("/tasks") {
+            header(
+                HttpHeaders.ContentType,
+                ContentType.Application.FormUrlEncoded.toString()
+            )
+            setBody(
+                listOf(
+                    "name" to "swimming",
+                    "description" to "Go to the beach",
+                    "priority" to "Low"
+                ).formUrlEncode()
+            )
+        }
+        assertEquals(HttpStatusCode.NoContent, response1.status)
+
+        val response2 = client.get("/tasks")
+        assertEquals(HttpStatusCode.OK, response2.status)
+        val body = response2.bodyAsText()
+
+        assertContains(body, "swimming")
+        assertContains(body, "Go to the beach")
     }
 }
